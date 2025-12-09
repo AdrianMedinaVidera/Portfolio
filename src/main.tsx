0f121bacf8503af09cd004c0452b78c5ea851536
Adrián Medina Videra <adrianmedinavidera@gmail.com>
2025-12-09 09:58:17 +0100

    Import LanguageProvider in main.tsx
    Added import for LanguageProvider from LanguageContext.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-// 1. IMPORTANTE: Importa el Provider
 import { LanguageProvider } from './LanguageContext'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
